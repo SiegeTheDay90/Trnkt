429ad7beb9c8019751426a79e79cc0d6302135db
cjsmi <ClarenceSmith90@gmail.com>
2022-10-31 18:05:44 -0400

    fix developer links

--- frontend/src/components/Navigation/DeveloperCard.jsx
+++ frontend/src/components/Navigation/DeveloperCard.jsx
@@ -9,20 +9,20 @@ const DeveloperCard = () => {
             <div className="dev-card-left">
                 <h3>Clarence Smith</h3>
 
-                <Link to="https://www.linkedin.com/in/clarence-smith-31b21b59/">
+                <a href="https://www.linkedin.com/in/clarence-smith-31b21b59/">
                     <img src="https://lh3.googleusercontent.com/a/ALm5wu0ouL35dm_Op3iQPlIf28Jge-dEZQt77vDyJXAk=s360-p-rw-no" alt="Developer Picture"/>
-                </Link>
+                </a>
             </div>
             <div className='dev-card-right'>
                 <h3>Contact Links</h3>
                 <ol>
-                    <li><Link to="https://github.com/SiegeTheDay90/Trnkt">Github</Link></li>
-                    <li><Link to="https://www.linkedin.com/in/clarence-smith-31b21b59/">LinkedIn</Link></li>
+                    <li><a href="https://github.com/SiegeTheDay90/Trnkt">Github</a></li>
+                    <li><a href="https://www.linkedin.com/in/clarence-smith-31b21b59/a">LinkedIn</a></li>
                 </ol>
                 <h3>Other Projects</h3>
                 <ol>
-                    <li><Link to="https://siegetheday90.github.io/Interceptor/">Interceptor (Game)</Link></li>
-                    <li><Link to="https://rising-sign.herokuapp.com/">Rising Sign (Social App)</Link></li>
+                    <li><a href="https://siegetheday90.github.io/Interceptor/">Interceptor (Game)</a></li>
+                    <li><a href="https://rising-sign.herokuapp.com/">Rising Sign (Social App)</a></li>
                 </ol>
             </div>
         </div>
